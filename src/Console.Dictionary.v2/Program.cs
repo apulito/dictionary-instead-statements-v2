534b8e07ebd3f1ed73ce762d4f0fab7e0e06b175
apulito <alberto.pulito@gmail.com>
2022-07-21 12:29:27 +0200

    Add basic skeleton of solution

---
+++ src/Console.Dictionary.v2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console.Dictionary.v2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ src/Console.Switch.v2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console.Switch.v2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
