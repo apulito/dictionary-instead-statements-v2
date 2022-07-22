a53d07738f66c3f02c53f4287bc9bb2c0efc15f7
apulito <alberto.pulito@gmail.com>
2022-07-22 11:37:15 +0200

    Add more enums for hit and ground types

---
+++ src/Domain.v2/FightingGroundType.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.v2;
+
+public enum FightingGroundType : int
+{
+    GROUND = 1,
+    AIR = 2,
+    WATER = 3
+}
---
+++ src/Domain.v2/HitType.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.v2;
+
+public enum HitType : int
+{
+    SUPER_WEAK = 1,
+    WEAK = 2,
+    EFFECTIVE = 3,
+    SUPER_EFFECTIVE = 4
+}
